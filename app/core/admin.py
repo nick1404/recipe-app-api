1f597252dcf4b8ff687cb98809fc1afc51b6391d
Nick Misiiuk <nickmisiiuk@MacBook-Pro-Volodymyr.local>
2020-08-05 12:55:58 +0300

    Add admin tests

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,1 +1,10 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from core import models
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['name', 'email']
+    
+    admin.site.register(models.User, UserAdmin)
