f602bc5d4cc2ad6f1d893b22e2b1e4d4c8b86f38
Nick Misiiuk <nickmisiiuk@MacBook-Pro-Volodymyr.local>
2020-08-13 12:47:16 +0300

    Re-factored test for tags and ingredients

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -68,3 +68,21 @@ class PrivateIngredientsAPITests(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
         self.assertEqual(response.data[0]['name'], ingredient.name)
+        
+    def test_create_ingredient_successful(self):
+        """Test creating an ingredient"""
+        payload = {'name': 'cabbage'}
+        self.client.post(INGREDIENTS_URL, payload)
+        
+        # Check that the ingredient has been created
+        exists = Ingredient.objects.filter(user=self.user, name=payload['name']).exists()
+        self.assertTrue(exists)
+        
+    def test_create_ingredient_invalid(self):
+        """Test creating invalid ingredient fails"""
+        payload = {'name': ''}
+        response = self.client.post(INGREDIENTS_URL, payload)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        
+        
+        
